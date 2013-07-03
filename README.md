dd9cd9ef55a7a3dcfb694b6e5a46d599d78bf938
Chris Jones <chris@jonezy.org>
2013-07-02 20:40:28 -0400

    initial check in
    Signed-off-by: Chris Jones <chris@jonezy.org>

---
+++ README.md
@@ -0,0 +1,3 @@
+# witness
+
+monitor websites with witness
---
+++ routes/index.js
@@ -0,0 +1,70 @@
+var nconf = require('nconf'),
+    http = require('http'),
+    async = require('async');
+
+
+// load the domains json file
+nconf.file({file: process.cwd() + '/domains.json' });
+
+var domains = nconf.get();
+var results = [];
+
+var resultTemplate = {
+  status: "ok",
+  cssClass: "alert-success"
+};
+
+exports.index = function(req, res){
+  var testDomains = [];
+  var i = 0;
+  for(var d in domains) {
+    testDomains[i] = domains[d];
+    i++;
+  }
+  async.forEachSeries(testDomains,
+  function(d, next) {
+    console.log('Testing ', d);
+    try {
+      doRequest(d, function(data) {
+        results[d] = data;
+        next();
+      });
+    } catch (e) {
+      next(null);
+    }
+  },function(err) {
+    if(err) console.log('ERROR ', err);
+    res.render('index', { title: 'Express', domains:domains, results: results });
+  });
+
+};
+
+
+function doRequest(url, cb) {
+  var options = {
+    host: url,
+    port: 80,
+    path: "/",
+    method: "GET"
+  };
+
+  // do the request!
+  var data = "";
+  var apiReq = http.request(options, function(apiRes) {
+    apiRes.setEncoding('utf8');
+    apiRes.on('data', function(chunk) {
+      data = resultTemplate;
+    });
+    apiRes.on('end', function() {
+      // execute the callback
+      cb && cb(data);
+    });
+  });
+
+  apiReq.on('error', function(e) {
+    data = { status: 'down', cssClass: 'alert-error' };
+    cb && cb(data);
+  });
+
+  apiReq.end();
+}
